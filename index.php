b74f49278ddb29d7b39264bfe00c2275ab1f44d6
Dakk17 <131455281+Dakk17@users.noreply.github.com>
2024-01-29 08:32:25 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,8 @@
+    <?php
+        include 'index.html';
+        $word = "Hello";
+        $int = 50;
+        $word2 = "World";
+        $final = $word.$int.$word2;
+        echo "<font color='blue'>".$final."</font>";
+    ?>
